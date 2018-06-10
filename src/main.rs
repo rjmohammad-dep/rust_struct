326981808d58c92bf8bf37f40527af0c19ca5810
RJ Mohammad <rana.mohammad@weightwatchers.com>
2018-06-10 02:49:26 -0400

    made area of rectangle function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let width1 = 30;
+    let height1 = 50;
+
+    println!(
+        " The Area of rectangle is {} square pixels",
+        area(width1, height1)
+    )
+}
+
+fn area(w: u32, h :u32) -> u32 {
+    w * h
 }
