0102dbc429e5a86e500577133894f3cab9dc781f
RJ Mohammad <rana.mohammad@weightwatchers.com>
2018-06-12 16:30:04 -0400

    implement an associate function

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,12 @@ impl Rectangle {
   fn can_hold(&self, other: &Rectangle) -> bool {
     self.width > other.width && self.height > other.height
   }
+  fn square(size: u32) -> Rectangle {
+    Rectangle {
+      width: size,
+      height: size
+    }
+  }
 }
 
 fn main() {
@@ -29,4 +35,5 @@ fn main() {
 
   println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
   println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+  println!("{:?}", Rectangle::square(12))
 }
